1cf0c47b92c5b1dfc9107f3f2290c5435622f9bd
egileekada <egileoniso.ekada@gmail.com>
2025-08-04 05:11:05 +0100

    update on product page

--- src/app/product/fundraising/page.tsx
+++ src/app/product/fundraising/page.tsx
@@ -1,19 +1,18 @@
 "use client"
 import { EVENT_PAGE_URL } from "@/helpers/services/urls";
-import { Flex } from "@chakra-ui/react";
-import { usePathname, useRouter } from "next/navigation";
-import { IoChevronBack } from "react-icons/io5";
+import { Flex, Image } from "@chakra-ui/react";
+import { useRouter } from "next/navigation"; 
 
 
 export default function Page() {
 
-    const router = useRouter() 
+    const router = useRouter()
     return (
         <Flex w={"full"} h={"100vh"} pos={"relative"} >
-            <Flex pos={"absolute"} onClick={() => router.push("/")} top={"8"} bgColor={"white"} cursor={"pointer"} justifyContent={"center"} alignItems={"center"} zIndex={"20"} left={"8"} w={"9"} h={"9"} rounded={"full"} borderWidth={"1px"} >
-                <IoChevronBack size={"25px"} color="black" />
-            </Flex> 
-            <iframe src={EVENT_PAGE_URL + "/product/fundraising?frame=true&theme=light"} width="100%" height="100%" ></iframe> 
+            <Flex pos={"absolute"} onClick={() => router.push("/")} top={"8"} bgColor={"transparent"} cursor={"pointer"} justifyContent={"center"} alignItems={"center"} zIndex={"20"} left={"8"} w={"9"} h={"9"} rounded={"full"} borderWidth={"0px"} >
+                <Image src={"/images/logo.png"} alt="logo" w={"60px"} h={"fit-content"} />
+            </Flex>
+            <iframe src={EVENT_PAGE_URL + "/product/fundraising?frame=true&theme=light"} width="100%" height="100%" ></iframe>
         </Flex>
     )
 }
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { EVENT_PAGE_URL } from "@/helpers/services/urls";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { IoChevronBack } from "react-icons/io5";
 
@@ -10,8 +10,8 @@ export default function Page() {
     const router = useRouter() 
     return (
         <Flex w={"full"} h={"100vh"} pos={"relative"} >
-            <Flex pos={"absolute"} onClick={() => router.push("/")} top={"8"} bgColor={"white"} cursor={"pointer"} justifyContent={"center"} alignItems={"center"} zIndex={"20"} left={"8"} w={"9"} h={"9"} rounded={"full"} borderWidth={"1px"} >
-                <IoChevronBack size={"25px"} color="black" />
+            <Flex pos={"absolute"} onClick={() => router.push("/")} top={"8"} bgColor={"transparent"} cursor={"pointer"} justifyContent={"center"} alignItems={"center"} zIndex={"20"} left={"8"} w={"9"} h={"9"} rounded={"full"} borderWidth={"0px"} >
+                <Image src={"/images/logo.png"} alt="logo" w={"60px"} h={"fit-content"} />
             </Flex> 
             <iframe src={EVENT_PAGE_URL + "/product/events?frame=true&theme=light"} width="100%" height="100%" ></iframe> 
         </Flex>
