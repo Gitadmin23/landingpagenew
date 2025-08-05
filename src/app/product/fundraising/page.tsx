5fff4a5c54fc8b4822458d73d685811af746069f
egileekada <egileoniso.ekada@gmail.com>
2025-08-05 10:13:18 +0100

    update on landing page product page

--- src/app/product/fundraising/page.tsx
+++ src/app/product/fundraising/page.tsx
@@ -6,13 +6,13 @@ import { useRouter } from "next/navigation";
 
 export default function Page() {
 
-    const router = useRouter()
+    const router = useRouter() 
     return (
         <Flex w={"full"} h={"100vh"} pos={"relative"} >
-            <Flex pos={"absolute"} onClick={() => router.push("/")} top={"8"} bgColor={"transparent"} cursor={"pointer"} justifyContent={"center"} alignItems={"center"} zIndex={"20"} left={"8"} w={"9"} h={"9"} rounded={"full"} borderWidth={"0px"} >
-                <Image src={"/images/logo.png"} alt="logo" w={"60px"} h={"fit-content"} />
-            </Flex>
-            <iframe src={EVENT_PAGE_URL + "/product/fundraising?frame=true&theme=light"} width="100%" height="100%" ></iframe>
+        <Flex pos={"absolute"} onClick={() => router.push("/")} top={"8"} bgColor={"transparent"} cursor={"pointer"} justifyContent={"center"} alignItems={"center"} zIndex={"20"} left={"8"} w={"9"} h={"9"} rounded={"full"} borderWidth={"0px"} >
+            <Image src={"/images/logo.png"} alt="logo" w={"60px"} h={"fit-content"} />
+        </Flex> 
+            <iframe src={EVENT_PAGE_URL + "/product/fundraising?frame=true&theme=light"} width="100%" height="100%" ></iframe> 
         </Flex>
     )
 }
