f73571bd796ca2f73c35246f457f6ff7f8383ff1
egileekada <egileoniso.ekada@gmail.com>
2025-08-28 14:45:05 +0100

    Add react-loader-spinner for enhanced loading experience in LogOutSpinner component

--- src/components/shared/logoutSpinner.tsx
+++ src/components/shared/logoutSpinner.tsx
@@ -6,6 +6,7 @@ import ModalLayout from "./modalLayout";
 import { signOut, useSession } from "next-auth/react";
 import useCustomTheme from "@/hooks/useTheme";
 import { useRouter } from "next/navigation";
+import { MutatingDots } from 'react-loader-spinner'
 
 
 export default function LogOutSpinner() { 
@@ -29,7 +30,17 @@ export default function LogOutSpinner() {
         <Flex w={"full"} h={"100vh"} bgColor={"white"} >
             <ModalLayout size="md" open={true} trigger={true} >
                 <Flex bgColor={"white"} w={"full"} h={"40vh"} justifyContent={"center"} alignItems={"center"} >
-                    <Spinner size={"lg"} color={primaryColor} />
+                <MutatingDots
+                    visible={true}
+                    height="100"
+                    width="100"
+                    color={primaryColor}
+                    secondaryColor={primaryColor}
+                    radius="12.5"
+                    ariaLabel="mutating-dots-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                />
                 </Flex>
             </ModalLayout>
         </Flex>
