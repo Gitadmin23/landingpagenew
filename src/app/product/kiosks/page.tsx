3f6f4e314e2af6a5be9ceed986bb702e6618efd6
egileekada <egileoniso.ekada@gmail.com>
2025-08-29 14:36:22 +0100

    Update iframe source URL in kiosks page and adjust navbar text size for better readability

--- src/app/product/kiosks/page.tsx
+++ src/app/product/kiosks/page.tsx
@@ -6,7 +6,7 @@ import { Flex } from "@chakra-ui/react";
 export default function Page() { 
     return (
         <Flex w={"full"} h={"100vh"} pos={"relative"} > 
-            <iframe src={DASHBOARDPAGE_URL + "/dashboard/product/kiosk?type=rental&frame=true&theme=light"} width="100%" height="100%" ></iframe>
+            <iframe src={DASHBOARDPAGE_URL + "/dashboard/product/kiosk?type=kiosk&frame=true&theme=light"} width="100%" height="100%" ></iframe>
         </Flex>
     )
 }
